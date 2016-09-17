228f9082388e11a218e837b72914220164b62af9
Martin <m.tichem@topdesk.com>
2016-09-17 22:21:28 +0200

    Add a bunch of sudokus to sudoku file

--- src/main/java/martinjo/sudoku/SudokuMain.java
+++ src/main/java/martinjo/sudoku/SudokuMain.java
@@ -3,7 +3,11 @@ package martinjo.sudoku;
 public class SudokuMain {
 
 	public static void main(String[] args) {
-		Sudoku sudoku = Sudoku.withPredefinedArray();
+//		Sudoku sudoku = Sudoku.threeStarSudoku();
+//		Sudoku sudoku = Sudoku.threeStarSudoku2();
+//		Sudoku sudoku = Sudoku.fourStarSudoku();
+		Sudoku sudoku = Sudoku.fiveStarSudoku();
+//		Sudoku sudoku = Sudoku.superHardSudoku();
 		sudoku.print();
 		System.out.println();
 		Solver solver = new Solver(sudoku);
