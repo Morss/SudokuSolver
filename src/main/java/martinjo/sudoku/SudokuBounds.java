988d4d953350ae215f81c06da2467758799bd8d4
Martin <m.tichem@topdesk.com>
2016-09-17 12:25:14 +0200

    SudokuBounds class extended with checks for row and col bounds

--- src/main/java/martinjo/sudoku/SudokuBounds.java
+++ src/main/java/martinjo/sudoku/SudokuBounds.java
@@ -1,15 +1,32 @@
 package martinjo.sudoku;
 
-public class SudokuBounds extends Exception {
-	private static final long serialVersionUID = 1L;
+public class SudokuBounds {
+	private static final int MAX_SIZE = 8;
+	private static final int MIN_SIZE = 0;
 	
 	public static void checkSudokuBounds(int row, int col) {
-		if (row < 0 || row > 8 || col < 0 || col > 9) {
-			try {
-				throw new OutOfSudokuBoundsException();
-			} catch (OutOfSudokuBoundsException e) {
-				e.printStackTrace();
-			}
+		if (row < MIN_SIZE || row > MAX_SIZE || col < MIN_SIZE || col > MAX_SIZE) {
+			throwCatchBoundsException();
+		}
+	}
+
+	public static void checkSudokuRowBounds(int row) {
+		if (row < MIN_SIZE || row > MAX_SIZE) {
+			throwCatchBoundsException();
+		}
+	}
+
+	public static void checkSudokuColBounds(int col) {
+		if (col < MIN_SIZE || col > MAX_SIZE) {
+			throwCatchBoundsException();
+		}
+	}
+
+	private static void throwCatchBoundsException() {
+		try {
+			throw new OutOfSudokuBoundsException();
+		} catch (OutOfSudokuBoundsException e) {
+			e.printStackTrace();
 		}
 	}
 	
